362a2ddcdc0dbd7a901584a05939d6f6212f3903
Tyler Flowers <ggtylerr_contact@protonmail.com>
2022-05-22 10:08:33 -0700

    Fix dmg

--- dmg-files/makedmg.sh
+++ dmg-files/makedmg.sh
@@ -7,8 +7,8 @@ create-dmg \
   --window-pos 200 120 \
   --window-size 512 384 \
   --icon-size 80 \
-  --icon "Revolt.app" 125 185 \
-  --hide-extension "Revolt.app" \
+  --icon "Divolt.app" 125 185 \
+  --hide-extension "Divolt.app" \
   --app-drop-link 375 185 \
   "Revolt-Installer.dmg" \
   "source_folder/"
